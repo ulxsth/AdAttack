b233281b88b098b0449934685c336a8a196278c9
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 14:27:35 +0900

    upd: Entityクラスを作成

---
+++ src/objects/Entity.js
@@ -0,0 +1,18 @@
+import { GameObject } from "./GameObject.js";
+import { GameState } from "../states/GameState.js";
+
+const gameState = GameState.getInstance();
+
+export class Entity extends GameObject {
+  constructor(x, y, width, height, color, hp, direction) {
+    super(x, y, width, height, color, direction);
+    this.hp = hp;
+  }
+
+  damage(damage) {
+    this.hp -= damage;
+    if (this.hp <= 0) {
+      gameState.removeObject(this);
+    }
+  }
+}
