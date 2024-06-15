8e7507f738764f2f633d8aef15c524126bf43381
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 10:49:53 +0900

    upd: キャンバスのレンダリング処理を追加

---
+++ src/Game.js
@@ -0,0 +1,27 @@
+export class Game {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.canvas.id = 'game';
+    this.canvas.style.backgroundColor = 'transparent';
+
+    this.context = this.canvas.getContext('2d');
+  }
+
+  init() {
+    document.body.appendChild(this.canvas);
+
+    this._render();
+  }
+
+  _draw() {
+    this.canvas.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this._render();
+  }
+
+  _render() {
+    window.requestAnimationFrame(this._draw.bind(this));
+  }
+}
