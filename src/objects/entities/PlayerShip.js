d6dadea6ded0079e52e51d5af611c22d9ac249c8
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 14:55:21 +0900

    upd: PlayerShipの移動処理を追加

--- src/objects/entities/PlayerShip.js
+++ src/objects/entities/PlayerShip.js
@@ -1,7 +1,33 @@
 import { Entity } from "../Entity.js";
+import { Game } from "../../Game.js";
+import { InteractionState } from "../../states/InteractionState.js";
+
+const game = Game.getInstance();
+const interactionState = InteractionState.getInstance();
 
 export class PlayerShip extends Entity {
   constructor(x, y, width, height, color, hp, direction) {
     super(x, y, width, height, color, hp, direction);
+  }
+
+  update() {
+    const { width: canvasWidth, height: canvasHeight } = game.getCenterOfCanvas();
+    const flags = interactionState.getAllFlags();
+
+    // 移動
+    if (!this.isFocusing) {
+      if (flags.up && this.y - PLAYER_SPEED >= 0) {
+        this.y -= PLAYER_SPEED;
+      }
+      if (flags.left && this.x - PLAYER_SPEED >= 0) {
+        this.x -= PLAYER_SPEED;
+      }
+      if (flags.down && this.y + PLAYER_SPEED + PLAYER_SHIP_HEIGHT <= canvasHeight) {
+        this.y += PLAYER_SPEED;
+      }
+      if (flags.right && this.x + PLAYER_SPEED + PLAYER_SHIP_WIDTH <= canvasWidth) {
+        this.x += PLAYER_SPEED;
+      }
+    }
   }
 }
