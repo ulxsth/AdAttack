ac2766b85c75979bb6930fcfbbd7549e6626dbfb
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 14:26:11 +0900

    upd: GameObject

---
+++ src/objects/GameObject.js
@@ -0,0 +1,34 @@
+export class GameObject {
+  constructor(x, y, width, height, color, direction) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.direction = direction;
+  }
+
+  update() {}
+
+  /**
+   * 重なっている部分があるかを検査する。
+   * @param {Object} obj
+   * @returns {boolean}
+   */
+  isCollidingWith = (obj) => {
+    // 長方形を (x..x+w), (y..y+h) のふたつの範囲として捉え、
+    // 2つのオブジェクトのそれぞれの範囲が重なっているかを考える
+
+    const isXOverlapped = this.x < (obj.x + obj.width) &&
+      (this.x + this.width) > obj.x ||
+      obj.x < (this.x + this.width) &&
+      (obj.x + obj.width) > this.x;
+
+    const isYOverlapped = this.y < (obj.y + obj.height) &&
+      (this.y + this.height) > obj.y ||
+      obj.y < (this.y + this.height) &&
+      (obj.y + obj.height) > this.y;
+
+    return isXOverlapped && isYOverlapped;
+  };
+}
