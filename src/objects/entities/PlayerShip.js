63311b0bea19706ded972df5e7b3e311caa042f4
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 15:11:08 +0900

    fix: 定数参照を削除

--- src/objects/entities/PlayerShip.js
+++ src/objects/entities/PlayerShip.js
@@ -2,8 +2,8 @@ import { Entity } from "../Entity.js";
 
 
 export class PlayerShip extends Entity {
-  constructor(x, y, width, height, color, hp, direction) {
-    super(x, y, width, height, color, hp, direction);
+  constructor(x, y, width, height, color, hp, direction, speed) {
+    super(x, y, width, height, color, hp, direction, speed);
   }
 
   update() {
@@ -11,19 +11,17 @@ export class PlayerShip extends Entity {
     const flags = this.interactionState.getAllFlags();
 
     // 移動
-    if (!this.isFocusing) {
-      if (flags.up && this.y - PLAYER_SPEED >= 0) {
-        this.y -= PLAYER_SPEED;
-      }
-      if (flags.left && this.x - PLAYER_SPEED >= 0) {
-        this.x -= PLAYER_SPEED;
-      }
-      if (flags.down && this.y + PLAYER_SPEED + PLAYER_SHIP_HEIGHT <= canvasHeight) {
-        this.y += PLAYER_SPEED;
-      }
-      if (flags.right && this.x + PLAYER_SPEED + PLAYER_SHIP_WIDTH <= canvasWidth) {
-        this.x += PLAYER_SPEED;
-      }
+    if (flags.up && this.y - this.speed >= 0) {
+      this.y -= this.speed;
+    }
+    if (flags.left && this.x - this.speed >= 0) {
+      this.x -= this.speed;
+    }
+    if (flags.down && this.y + this.speed + this.height <= canvasHeight) {
+      this.y += this.speed;
+    }
+    if (flags.right && this.x + this.speed + this.width <= canvasWidth) {
+      this.x += this.speed;
     }
   }
 }
