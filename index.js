3496c69a1fd9dae18a94873f08a59b325af46e19
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 11:48:08 +0900

    upd: スクリプトを停止できるようにした（邪魔なので）

--- index.js
+++ index.js
@@ -4,3 +4,12 @@
   game.init();
 })()
 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "stop") {
+    var element = document.getElementById('game');
+    if (element) {
+      element.style.display = 'none';
+    }
+  }
+  console.log("test");
+});
