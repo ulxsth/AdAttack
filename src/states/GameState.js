a604f045df276f820f87e448fd28197117cc314d
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 12:20:31 +0900

    upd: ステータスを変更するgetter,setterを作成

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -6,6 +6,18 @@ export class GameState {
     this.gameStatus = gameStatus.playing;
   }
 
+  get gameStatus() {
+    return this._gameStatus;
+  }
+
+  set gameStatus(status) {
+    if (!Object.values(gameStatus).includes(status)) {
+      throw new Error("Invalid game status");
+    }
+    
+    this._gameStatus = status;
+  }
+
   update() {
     this.objects.forEach(object => {
       object.update();
