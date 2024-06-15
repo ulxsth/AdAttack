9ad6cf273c1fd60c998c818e567fc64a1c9b5045
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 15:51:27 +0900

    upd: PlayerBullet を追加

---
+++ src/objects/bullets/PlayerBullet.js
@@ -0,0 +1,15 @@
+import { Bullet } from "../Bullet.js";
+
+export class PlayerBullet extends Bullet {
+  constructor(x, y, damage) {
+    super(x, y, 1, 1, "blue", 0, damage, 0);
+    this.existTime = 3;
+  }
+
+  update() {
+    this.existTime--;
+    if (this.existTime <= 0) {
+      this.game.gameState.removeObject(this);
+    }
+  }
+}
