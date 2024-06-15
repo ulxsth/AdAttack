d1f836974d2067b1d325427f4920e15cc0f424eb
yotu <114195789+ulxsth@users.noreply.github.com>
2024-06-15 11:54:49 +0900

    upd: オンオフ切り替え可能にした

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,4 +1,11 @@
+const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
+
+startBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "start" });
+  });
+});
 
 stopBtn.addEventListener('click', function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-(async () => {
-  const { Game } = await import(chrome.runtime.getURL('src/Game.js'));
-  const game = new Game();
-  game.init();
-})()
+
+// ゲームを起動する
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "start") {
+    (async () => {
+      const { Game } = await import(chrome.runtime.getURL('src/Game.js'));
+      const game = new Game();
+      game.init();
+    })()
+  }
+});
 
 // ゲームを停止する（canvasの削除）
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
